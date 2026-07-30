03922f2e2ac02fa0ea2a78317a1806cf842e4be2
yelsojuupendrachary <yelusojuupendrachary@gmail.com>
2026-07-30 14:50:14 +0530

    feat: base structure of url shortener

---
+++ ai_prof_url_shortener/src/main/java/ai_pro_url_shortener/AiProApplication.java
@@ -0,0 +1,13 @@
+package ai_pro_url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProApplication.class, args);
+	}
+
+}
---
+++ ai_prof_url_shortener/src/test/java/ai_pro_url_shortener/AiProApplicationTests.java
@@ -0,0 +1,13 @@
+package ai_pro_url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
